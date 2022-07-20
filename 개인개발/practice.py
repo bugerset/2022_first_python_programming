5a9464f78e28b69a2fdceab06fe5a1c76790e826
bugerset <youngseo1114@naver.com>
2022-07-21 00:58:52 +0900

    ㅂ

--- 개인개발/practice.py
+++ 개인개발/practice.py
@@ -1,11 +1,26 @@
-n = int(input())
-result = 0
-for i in range(1,n+1):
-    A = list(map(int,str(i)))
-    result = i + sum(A)
-    if result == n:
-        print(i)
-        break
+import sys
+n = int(sys.stdin.readline())
 
-    if i == n:
-        print(0)
+temp = True
+stack = []
+answer = []
+count = 1
+
+for i in range(n):
+    num = int(input())
+    while count <= num: # 입력된 수열보다 작은 경우 같아질 때까지 반복
+        stack.append(count)
+        answer.append('+')
+        count += 1
+    if stack[-1] == num: # 입력된 수열과 동일한 경우
+        stack.pop()
+        answer.append('-')
+    else: # 특정 수열을 만들 수 없을 경우
+        temp = False
+    print(answer)
+        
+if not temp:
+    print('NO')
+else:
+    for i in answer:
+        print(i)
